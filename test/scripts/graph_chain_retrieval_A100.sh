ba5cd8ec46662f927fa3bd26a268e023b6f18acf
Hanshi Sun <hanshis@andrew.cmu.edu>
2024-02-26 13:07:34 -0500

    update

--- test/scripts/graph_chain_retrieval_A100.sh
+++ test/scripts/graph_chain_retrieval_A100.sh
@@ -1,8 +1,8 @@
-python test/graph_chain_retrieval.py  --gamma 5 --prefill 65536 --budget 0.05 --dataset pg19 --log_csv
-python test/graph_chain_retrieval.py  --gamma 5 --prefill 98304 --budget 0.05 --dataset pg19 --log_csv
-python test/graph_chain_retrieval.py  --gamma 5 --prefill 114688 --budget 0.05 --dataset pg19 --log_csv
-python test/graph_chain_retrieval.py  --gamma 5 --prefill 65536 --budget 0.05 --dataset pg19 --log_csv
-python test/graph_chain_retrieval.py  --gamma 5 --prefill 98304 --budget 0.05 --dataset pg19 --log_csv
-python test/graph_chain_retrieval.py  --gamma 5 --prefill 114688 --budget 0.05 --dataset pg19 --log_csv
-python test/graph_chain_retrieval.py  --gamma 5 --prefill 65536 --budget 0.05 --dataset pg19 --log_csv
-python test/graph_chain_retrieval.py  --gamma 5 --prefill 98304 --budget 0.05 --dataset pg19 --log_csv
+CUDA_VISIBLE_DEVICES=0 python test/graph_chain_retrieval.py  --gamma 5 --prefill 65536 --budget 0.05 --dataset pg19 --log_csv
+CUDA_VISIBLE_DEVICES=1 python test/graph_chain_retrieval.py  --gamma 5 --prefill 98304 --budget 0.05 --dataset pg19 --log_csv
+CUDA_VISIBLE_DEVICES=2 python test/graph_chain_retrieval.py  --gamma 5 --prefill 114688 --budget 0.05 --dataset pg19 --log_csv
+CUDA_VISIBLE_DEVICES=3 python test/graph_chain_retrieval.py  --gamma 5 --prefill 65536 --budget 0.05 --dataset pg19 --log_csv
+CUDA_VISIBLE_DEVICES=4 python test/graph_chain_retrieval.py  --gamma 5 --prefill 98304 --budget 0.05 --dataset pg19 --log_csv
+CUDA_VISIBLE_DEVICES=5 python test/graph_chain_retrieval.py  --gamma 5 --prefill 114688 --budget 0.05 --dataset pg19 --log_csv
+CUDA_VISIBLE_DEVICES=6 python test/graph_chain_retrieval.py  --gamma 5 --prefill 65536 --budget 0.05 --dataset pg19 --log_csv
+CUDA_VISIBLE_DEVICES=7 python test/graph_chain_retrieval.py  --gamma 5 --prefill 98304 --budget 0.05 --dataset pg19 --log_csv
